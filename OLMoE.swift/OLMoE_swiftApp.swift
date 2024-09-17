54a89e9cd92ed82bfa2027563f9fdc0a9b3c11e2
Luca Soldaini <lucas@allenai.org>
2024-09-16 18:11:09 -0700

    Initial Commit

---
+++ OLMoE.swift/OLMoE_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  OLMoE_swiftApp.swift
+//  OLMoE.swift
+//
+//  Created by Luca Soldaini on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct OLMoE_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
