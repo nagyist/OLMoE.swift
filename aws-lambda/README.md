328ddcf31d71e799e75187099164225f73526348
Luca Soldaini <lucas@allenai.org>
2024-09-24 12:42:48 -0700

    finished lambda

--- aws-lambda/README.md
+++ aws-lambda/README.md
@@ -17,11 +17,91 @@ pip install --upgrade aws-sam-cli
 First, build the image:
 
 ```shell
-sam build
+AWS_PROFILE=llm sam build
 ```
 
 Then, run the lambda locally:
 
 ```shell
 AWS_PROFILE=llm sam local invoke S3LoggingFunction -e tests/test_event.json
+```
+
+## Deploy
+
+If you have not deployed before, you will need to deploy:
+
+```shell
+AWS_PROFILE=llm sam deploy --guided
+```
+
+After that, you can deploy changes with:
+
+```shell
+AWS_PROFILE=llm sam deploy
+```
+
+## API Gateway Spec
+
+You can log traces from any application using the API Gateway endpoint.
+
+- **Method**: POST
+- **URL**: https://ziv3vcg14i.execute-api.us-east-1.amazonaws.com/prod
+- **Body**:
+
+```json
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "required": ["model", "created", "messages"],
+  "properties": {
+    "model": {
+      "type": "string",
+      "description": "The model name"
+    },
+    "created": {
+      "type": "integer",
+      "description": "Unix timestamp of when the trace was created"
+    },
+    "system_fingerprint": {
+      "type": "string",
+      "description": "The fingerprint of the system. If not provided, the model will be used."
+    },
+    "id": {
+      "type": "string",
+      "description": "The ID of the trace. If not provided, the server will assign a random UUID by the endpoint. Must be unique across all traces with the same fingerprint."
+    },
+    "object": {
+      "type": "string",
+      "default": "chat.trace",
+      "description": "The object type. Defaults to `chat.trace`."
+    },
+    "messages": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "required": ["role", "content"],
+        "properties": {
+          "role": {
+            "type": "string",
+            "enum": ["system", "user", "assistant"],
+            "description": "The role should be one of system, user, or assistant."
+          },
+          "content": {
+            "type": "string",
+            "description": "The content of the message."
+          }
+        }
+      },
+      "description": "An array of message objects."
+    },
+    "choices": {
+      "type": "object",
+      "description": "Field for compatibility with OpenAI's API."
+    },
+    "usage": {
+      "type": "object",
+      "description": "Field for compatibility with OpenAI's API."
+    }
+  }
+}
 ```
--- aws-lambda/src/lambda_function.py
+++ aws-lambda/src/lambda_function.py
@@ -1,7 +1,6 @@
 import json
-import traceback
 from dataclasses import dataclass, field
-import boto3
+import boto3  # type: ignore
 import uuid
 from datetime import datetime
 
@@ -34,38 +33,41 @@ class Message:
 
 
 @dataclass
-class Metadata:
-    # this is something like "iPhone15,2"
-    device_id: str
-    timestamp: datetime
-    model_id: str
-    server_id: str = field(default_factory=lambda: str(uuid.uuid4()))
-
-    def __post_init__(self):
-        assert self.device_id is not None, "Device ID is required"
-        assert self.server_id is not None, "Server ID is required"
-        assert self.timestamp is not None, "Timestamp is required"
-        self.timestamp = datetime.fromisoformat(self.timestamp)
-        assert self.model_id is not None, "Model ID is required"
-
-    def to_dict(self):
-        return {
-            "device_id": self.device_id,
-            "server_id": self.server_id,
-            "model_id": self.model_id,
-            "timestamp": self.timestamp.isoformat(),
-        }
-
-
-@dataclass
-class LogEntry:
+class Trace:
+    created: int
+    model: str
     messages: list[Message]
-    metadata: Metadata
+
+    # default is to have server assign ID and object
+    id: str = field(default_factory=lambda: str(uuid.uuid4()))
+    object: str = "chat.trace"
+
+    # we will derive this from the model if not provided
+    system_fingerprint: str | None = None
+
+    # these two are for compatibility with the openai format
+    choices: list[dict] = field(default_factory=list)
+    usage: dict = field(default_factory=dict)
 
     def __post_init__(self):
         assert len(self.messages) > 0, "Messages are required"
         self.messages = [Message(**message) for message in self.messages]
-        self.metadata = Metadata(**self.metadata)
+
+        self.system_fingerprint = self.system_fingerprint or self.model
+
+        assert isinstance(self.created, int), "Created must be an int"
+        try:
+            datetime.fromtimestamp(self.created)
+        except (ValueError, TypeError, OverflowError):
+            raise ValueError(f"Invalid timestamp: {self.created}")
+
+        assert isinstance(self.choices, list), "Choices must be a list"
+        assert all(isinstance(choice, dict) for choice in self.choices), "Each choice must be a dict"
+        assert isinstance(self.usage, dict), "Usage must be a dict"
+        assert isinstance(self.model, str), "Model must be a string"
+        assert isinstance(self.id, str), "ID must be a string"
+        assert isinstance(self.object, str), "Object must be a string"
+        assert isinstance(self.system_fingerprint, str), "System fingerprint must be a string"
 
     @classmethod
     def from_dict(cls, log: dict):
@@ -74,51 +76,51 @@ class LogEntry:
     def to_dict(self):
         return {
             "messages": [message.to_dict() for message in self.messages],
-            "metadata": self.metadata.to_dict(),
+            "created": self.created,
+            "model": self.model,
+            "id": self.id,
+            "object": self.object,
+            "system_fingerprint": self.system_fingerprint,
+            **({"choices": self.choices} if self.choices else {}),
+            **({"usage": self.usage} if self.usage else {}),
         }
 
 
 def lambda_handler(event, context):
     try:
+        # get the body from the event
+        # body = json.loads(event.get("body", "{}"))
+        body = event
         # Extract logs from the event
-        logs = event.get("logs", [])
-        parsed_logs = [LogEntry.from_dict(log) for log in logs]
-        url = write_to_s3(parsed_logs)
+        parsed_log = Trace.from_dict(body)
+        url = write_to_s3(parsed_log)
         status_code = 200
-        body = json.dumps(
-            {
-                "outcome": "success",
-                "error": None,
-                "url": url,
-            }
-        )
+        body = {
+            "outcome": "success",
+            "error": None,
+            "url": url,
+        }
     except Exception as e:
         status_code = 500
-        body = json.dumps(
-            {
-                "outcome": type(e).__name__,
-                "error": f"{str(e)}: {traceback.format_exc()}",
-                "url": None,
-            }
-        )
+        body = {
+            "outcome": "failure",
+            "error": f"{type(e).__name__}: {e}",
+            "url": None,
+        }
 
-    return {"statusCode": status_code, "body": body}
+    return {"statusCode": status_code, "body": json.dumps(body)}
 
 
-def write_to_s3(logs: list[LogEntry]):
-    # Generate a unique filename
-    for log in logs:
-        # Get the date from the log timestamp
-        date_prefix = log.metadata.timestamp.date().strftime("%Y%m%d")
-        data_key = f"{S3_LOG_PREFIX}/{date_prefix}/{log.metadata.server_id}.json"
-        # Convert batch to JSON string
-        s3.put_object(
-            Bucket=BUCKET_NAME, Key=data_key, Body=json.dumps(log.to_dict()), ContentType="application/json"
-        )
+def write_to_s3(log: Trace):
+    # Get the date from the log timestamp
+    date_prefix = datetime.fromtimestamp(log.created).date().strftime("%Y%m%d")
+    data_key = f"{S3_LOG_PREFIX}/{log.system_fingerprint}/{date_prefix}/{log.id}.json"
+    # Convert batch to JSON string
+    s3.put_object(Bucket=BUCKET_NAME, Key=data_key, Body=json.dumps(log.to_dict()), ContentType="application/json")
 
-        # render html
-        html = CHAT_TEMPLATE.replace("[[ADD_JSON_HERE]]", json.dumps(log.to_dict()))
-        html_key = f"{S3_SHARE_PREFIX}/{date_prefix}/{log.metadata.server_id}.html"
-        s3.put_object(Bucket=BUCKET_NAME, Key=html_key, Body=html, ContentType="text/html")
+    # render html
+    html = CHAT_TEMPLATE.replace("[[ADD_JSON_HERE]]", json.dumps(log.to_dict()))
+    html_key = f"{S3_SHARE_PREFIX}/{log.system_fingerprint}/{date_prefix}/{log.id}.html"
+    s3.put_object(Bucket=BUCKET_NAME, Key=html_key, Body=html, ContentType="text/html")
 
-        return f"https://{BUCKET_NAME}.s3.amazonaws.com/{html_key}"
+    return f"https://{BUCKET_NAME}.s3.amazonaws.com/{html_key}"
