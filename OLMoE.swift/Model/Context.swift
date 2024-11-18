c9957b9a3c485628d895bd089684c467de569c17
Ken Adamson <ken.adamson@genui.com>
2024-11-17 17:59:36 -0800

    Reorganized and slightly reformatted

---
+++ OLMoE.swift/Model/Context.swift
@@ -0,0 +1,23 @@
+//
+//  Context.swift
+//  OLMoE.swift
+//
+//  Created by Ken Adamson on 11/17/24.
+//
+
+
+import Foundation
+import llama
+
+public class Context {
+    let pointer: OpaquePointer
+    init(_ model: Model, _ params: llama_context_params) {
+        self.pointer = llama_new_context_with_model(model, params)
+    }
+    deinit {
+        llama_free(pointer)
+    }
+    func decode(_ batch: llama_batch) {
+        guard llama_decode(pointer, batch) == 0 else { fatalError("llama_decode failed") }
+    }
+}
