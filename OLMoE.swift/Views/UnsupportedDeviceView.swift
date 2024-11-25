03e4127fc33bcceff6d417957efd18c873dcebf9
Jon Ryser <241263+jonryser@users.noreply.github.com>
2024-11-25 11:47:57 -0800

    Updated UnsupportedDeviceView with buttons and proper widths.

--- OLMoE.swift/Views/UnsupportedDeviceView.swift
+++ OLMoE.swift/Views/UnsupportedDeviceView.swift
@@ -11,9 +11,11 @@ import os
 import UIKit
 
 struct UnsupportedDeviceView: View {
-
     let proceedAnyway: () -> Void
     let proceedMocked: () -> Void
+
+    @State private var mockedModelButtonWidth: CGFloat = 100
+    @State private var notSupportedWidth: CGFloat = 100
 
     var body: some View {
         let availableMemoryInGB = Double(os_proc_available_memory()) / (1024 * 1024 * 1024)
@@ -24,10 +26,17 @@ struct UnsupportedDeviceView: View {
                 .foregroundColor(Color("AccentColor"))
 
             Text("Device Not Supported")
+                .id(UUID()) // Force unique ID so onAppear gets updated width
                 .font(.title())
                 .foregroundColor(Color("AccentColor"))
+                .background(GeometryReader { geometry in
+                    Color.clear.onAppear {
+                        notSupportedWidth = geometry.size.width + 24
+                    }
+                })
 
             Text("This app requires a device with at least 8GB of RAM.")
+                .frame(width: notSupportedWidth)
                 .multilineTextAlignment(.center)
                 .padding([.horizontal], 32)
                 .padding([.vertical], 2)
@@ -36,6 +45,7 @@ struct UnsupportedDeviceView: View {
             if FeatureFlags.allowDeviceBypass {
                 if availableMemoryInGB > 0 {
                     Text("(The model requires ~6 GB and this device has: \(formattedMemory) GB available.)")
+                        .frame(width: notSupportedWidth)
                         .multilineTextAlignment(.center)
                         .padding()
                         .font(.body())
@@ -44,16 +54,22 @@ struct UnsupportedDeviceView: View {
                 Button("Proceed Anyway") {
                     proceedAnyway()
                 }
+                .buttonStyle(PrimaryButton(minWidth: mockedModelButtonWidth))
                 .padding(.vertical, 5)
-                .font(.body())
             }
 
             if FeatureFlags.allowMockedModel {
                 Button("Proceed With Mocked Model") {
                     proceedMocked()
                 }
+                .id(UUID()) // Force unique ID so onAppear gets updated width
+                .buttonStyle(.PrimaryButton)
                 .padding(.vertical, 5)
-                .font(.body())
+                .background(GeometryReader { geometry in
+                    Color.clear.onAppear {
+                        mockedModelButtonWidth = geometry.size.width - 24
+                    }
+                })
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -71,6 +87,5 @@ struct UnsupportedDeviceView: View {
         }
     )
     .preferredColorScheme(.dark)
-    .padding()
     .background(Color("BackgroundColor"))
 }
