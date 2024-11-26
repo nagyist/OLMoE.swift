16f1d622a04f68a74679cb255317d82368e84a87
Stanley Jovel <luigi.lsjp@gmail.com>
2024-11-25 16:55:51 -0800

    Create new function requestChallenge and incorporate it to the perform attest procedure

--- OLMoE.swift/Model/AttestManager.swift
+++ OLMoE.swift/Model/AttestManager.swift
@@ -15,7 +15,49 @@ class AppAttestManager {
         let attestationObjectBase64: String
     }
     
-    static func performAttest(challengeString: String) async throws -> AttestationResult {
+    static func requestChallenge(keyID: String) async throws -> String? {
+        #if targetEnvironment(simulator)
+            throw NSError(domain: "AppAttest", code: -1, userInfo: [NSLocalizedDescriptionKey: "App Attest not supported on simulator."])
+        #else
+        let jsonData = try JSONSerialization.data(withJSONObject: [
+            "key_id": keyID
+        ])
+
+        guard let url = URL(string: Configuration.apiUrl), !Configuration.apiUrl.isEmpty else {
+            throw NSError(domain: "AppAttest", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(Configuration.apiKey, forHTTPHeaderField: "x-api-key")
+        request.httpBody = jsonData
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NSError(domain: "AppAttest", code: -1, userInfo: [NSLocalizedDescriptionKey: "Failed to request challenge"])
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            let responseModel = try decoder.decode(LambdaResponseModel.self, from: data)
+            
+            guard let challenge = responseModel.body.challenge else {
+                print("Challenge not found in response")
+                return nil
+            }
+            
+            print("Challenge: \(challenge)")
+            return challenge
+        } catch {
+            print("Failed to decode JSON: \(error.localizedDescription)")
+            return nil
+        }
+        
+        #endif
+    }
+    
+    static func performAttest() async throws -> AttestationResult {
         #if targetEnvironment(simulator)
             throw NSError(domain: "AppAttest", code: -1, userInfo: [NSLocalizedDescriptionKey: "App Attest not supported on simulator."])
         #else
@@ -25,23 +67,14 @@ class AppAttestManager {
             throw NSError(domain: "AppAttest", code: -1, userInfo: [NSLocalizedDescriptionKey: "App Attest not supported on this device."])
         }
 
-        let clientDataHash = Data(SHA256.hash(data: Data(challengeString.utf8)))
-
         // Generate a new key if none exists
-        let keyID: String = try await withCheckedThrowingContinuation { continuation in
-            service.generateKey { newKeyID, error in
-                if let error = error {
-                    continuation.resume(throwing: error)
-                } else if let newKeyID = newKeyID {
-                    continuation.resume(returning: newKeyID)
-                } else {
-                    continuation.resume(throwing: NSError(domain: "AppAttest", code: -1, userInfo: nil))
-                }
-            }
+        guard let challenge = try await requestChallenge(keyID: keyID) else {
+            throw NSError(domain: "AppAttest", code: -1, userInfo: [NSLocalizedDescriptionKey: "Failed to request challenge"])
         }
-
+    
+        let clientDataHash = Data(SHA256.hash(data: Data(challenge.utf8)))
+        
         let attestationObject: Data = try await withCheckedThrowingContinuation { continuation in
-            // attestation happens here
             service.attestKey(keyID, clientDataHash: clientDataHash) { attestation, error in
                 if let error = error {
                     continuation.resume(throwing: error)
